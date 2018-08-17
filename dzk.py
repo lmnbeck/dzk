39c138bbbf0e873fe48fd5f35ad6a46e467c5d94
lmnbeck <maniagea09@gmail.com>
2018-08-17 17:14:07 +0800

    add classes

--- dzk.py
+++ dzk.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+from zk import *
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 background_image = 'image/bg.jpg'
 stick_image = 'image/head.jpg'
+ball_image = 'image/ball.png'
+bluezk_image = 'image/blueZK.png'
 
 # 初始化pygame，为使用硬件做准备
 pygame.init()
 # 创建了一个窗口
-screen = pygame.display.set_mode((480, 600), 0, 32)
+bg_size = (480,600)
+screen = pygame.display.set_mode(bg_size, 0, 32)
 # 设置窗口标题
 pygame.display.set_caption("Happy 7c")
 
 # 刷新频率
 fpsClock = pygame.time.Clock()
-FPS = 30
+FPS = 60
 
 # 加载并转换图像
 background = pygame.image.load(background_image).convert()
-stick_cursor = pygame.image.load(stick_image).convert_alpha()
+
+# 球砖杆
+ball_speed = [0,0]
+ball = Ball(ball_image,(140,360),bg_size)
+bluezk = ZK(bluezk_image,(100,100),bg_size)
+stick = Stick(stick_image,(140,400),bg_size)
 
 # 方向
-direct_x = 0
-direct_y = 0
+direct_x = (0,0)
+Colide_direct = 1
 keyPressed = False
 
 # 杆坐标
-stick_x = screen.get_rect().centerx - stick_cursor.get_rect().width/2
-stick_y = screen.get_rect().bottom - stick_cursor.get_rect().height
+stick_x = screen.get_rect().centerx - stick.getImage().get_rect().width/2
+stick_y = screen.get_rect().bottom - stick.getImage().get_rect().height
 print(stick_x,stick_y)
+stick_width = stick.getImage().get_rect().width
 
-stick_width = stick_cursor.get_rect().width
-
+zkGroup = pygame.sprite.Group()
+zkGroup.add(bluezk)
 
 while True:
     for event in pygame.event.get():
@@ -48,42 +59,65 @@ while True:
             # if(event.key == K_DOWN or event.key == K_s):
             #     direct_y = 1
             if(event.key == K_LEFT or event.key == K_a):
-                direct_x = -1
+                direct_x = (-3,0)
             if(event.key == K_RIGHT or event.key == K_d):
-                direct_x = 1
+                direct_x = (3,0)
+            if(event.key == K_SPACE):
+                ball_speed = [random.choice([-3,3]),-3]
+                
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+
         if event.type == KEYUP:
             keyPressed = False
-            direct_x = 0
-            direct_y = 0
-    
-    if keyPressed == True:
-        if direct_x > 0:
-            direct_x += 1
-        elif direct_x < 0:
-            direct_x -= 1
-        stick_x += direct_x
-        stick_y += direct_y
+            direct_x = (0,0)
+    # 杆加速
+    # if keyPressed == True:
+    #     if direct_x[0] > 0:
+    #         direct_x = (direct_x[0]+1,0)
+    #     elif direct_x[0] < 0:
+    #         direct_x = (direct_x[0]-1,0)
 
-    if stick_x <= 0:
-        stick_x = 0
-    elif stick_x > screen.get_width() - stick_cursor.get_rect().width:
-        stick_x = screen.get_width()- stick_cursor.get_rect().width
-    if stick_y <= 0:
-        stick_y = 0
-    elif stick_y > screen.get_height():
-        stick_y = screen.get_height()
+    # 杆移动
+    stick.move(direct_x)
+
+    # 球设定
+    Colide_direct = 0
+    #ball_speed = ball.move(ball_speed,0)
+
+    # 碰撞检测
+    # 碰砖
+    for each in zkGroup:
+        if pygame.sprite.collide_mask(ball, bluezk):
+            print("peng zhao le!!!")
+            bluezk.setVisable(False)
+            Colide_direct = 1
+            zkGroup.remove(each)
+            # if zkGroup.sprites():
+            #     # continue
+            # else:
+            #     # win the game
+    # 碰杆
+    if pygame.sprite.collide_mask(ball, stick):
+        Colide_direct = 2
+
+    ball_speed = ball.move(ball_speed,Colide_direct)
+
     screen.blit(background, (0, 0))  # 画上背景图
 
     # direct_x, direct_y = pygame.mouse.get_pos()  # 获得鼠标位置
     # # 计算光标左上角位置
-    # direct_x -= stick_cursor.get_width()/2
-    # direct_y -= stick_cursor.get_height()/2
+    # direct_x -= stick.getImage().get_width()/2
+    # direct_y -= stick.getImage().get_height()/2
 
     # 画上光标
-    screen.blit(stick_cursor, (stick_x, stick_y))
+    screen.blit(stick.getImage(), stick.getPosition())
+    screen.blit(ball.getImage(), ball.getPosition())
+    if bluezk.getVisable():
+        screen.blit(bluezk.getImage(), bluezk.getPosition())
+
+
 
     # 刷新画面
     pygame.display.update()
--- zk.py
+++ zk.py
@@ -1,1 +1,99 @@
 
+import pygame
+import math
+import sys
+from pygame.locals import *
+from random import *
+
+class Ball(pygame.sprite.Sprite):
+	def __init__(self, image, position, bg_size):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.image.load(image).convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = position
+		self.width, self.height = bg_size[0], bg_size[1]
+
+	def move(self,speed,direct):
+		
+		# 撞墙反弹
+		if self.rect.left < 0 or self.rect.right > self.width:
+			speed = (-speed[0],speed[1])
+		if self.rect.top < 0:
+			speed = (speed[0],-speed[1])
+		# if self.rect.top < 0:
+		# 	speed = (speed[0],-speed[1])
+
+		# 撞物反弹
+		if direct == 1:	#上
+			speed = (speed[0], -speed[1])
+		if direct == 2:	#下
+			speed = (speed[0], -speed[1])
+		if direct == 3:	#左
+			speed = (-speed[0], speed[1])
+		if direct == 4:	#右
+			speed = (-speed[0], speed[1])
+
+
+		self.rect = self.rect.move(speed)
+
+		return speed
+
+
+	def collide(self):
+		pass
+
+	def getImage(self):
+		return self.image
+	def getPosition(self):
+		return (self.rect.left,self.rect.top)
+
+class ZK(pygame.sprite.Sprite):
+	"""docstring for zk"""
+	def __init__(self, image, position, bg_size):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.image.load(image).convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = position
+		self.width, self.height = bg_size[0], bg_size[1]
+		self.visable = True
+	def collide(self):
+		pass
+
+	def getImage(self):
+		return self.image
+	def getPosition(self):
+		return (self.rect.left,self.rect.top)
+	def setVisable(self, visableFlag):
+		self.visable = visableFlag
+	def getVisable(self):
+		return self.visable
+
+class Stick(pygame.sprite.Sprite):
+	"""docstring for zk"""
+	def __init__(self, image, position, bg_size):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.image.load(image).convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = position
+		self.width, self.height = bg_size[0], bg_size[1]
+		
+	def move(self, speed):
+		# 撞墙停止
+		if self.rect.left < 0:
+			self.rect.left = 0
+		if self.rect.right > self.width:
+			self.rect.right = self.width
+
+		self.rect = self.rect.move(speed)
+		return speed
+
+	def collide(self):
+		pass
+
+	def getImage(self):
+		return self.image
+	def getPosition(self):
+		return (self.rect.left,self.rect.top)
